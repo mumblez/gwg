16c3b9273fdcd285b82a1e78f6f9f0a29a5c365e
Yusuf Tran <yusuf.tran@karhoo.com>
2018-01-25 18:16:37 +0000

    bla

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := "hello"
+	fmt.Println("hello -1")
+
+	x = "bye"
+	fmt.Println("hello -2")
+	x = "222"
+	fmt.Println("hello -3")
+	x = "555"
+	fmt.Println(x)
+}
