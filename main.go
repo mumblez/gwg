3a36046e040e18d753996ec5f4de9df80d2d84f3
Yusuf Tran <yusuf.tran@karhoo.com>
2018-01-26 00:12:45 +0000

    rubbish

--- main.go
+++ main.go
@@ -5,11 +5,12 @@ import "fmt"
 func main() {
 	x := "hello"
 	fmt.Println("hello -1")
-
 	x = "bye"
 	fmt.Println("hello -2")
 	x = "222"
-	fmt.Println("hello -3")
+	fmt.Pzzrintln("hello -3")
 	x = "555"
+	bla
+
 	fmt.Println(x)
 }
