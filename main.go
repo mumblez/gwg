41d0fbdac2fe45b025df273452bba47980eb1196
Yusuf Tran <yusuf.tran@karhoo.com>
2018-01-26 23:54:12 +0000

    poc

--- main.go
+++ main.go
@@ -1,12 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
 
+	"gopkg.in/src-d/go-git.v4"
+	. "gopkg.in/src-d/go-git.v4/_examples"
+)
+
+// Pull changes from a remote repository
 func main() {
-	x := "hello"
-	fmt.Println("hello -1")
-	x = "bye"
-	fmt.Println("hello -2")
-	x = "222"
-	fmt.Println(x)
+	CheckArgs("<path>")
+	path := os.Args[1]
+
+	// We instance a new repository targeting the given path (the .git folder)
+	r, err := git.PlainOpen(path)
+	CheckIfError(err)
+
+	// Get the working directory for the repository
+	w, err := r.Worktree()
+	CheckIfError(err)
+
+	// Pull the latest changes from the origin remote and merge into the current branch
+	// Info("git pull origin")
+	// err = w.Pull(&git.PullOptions{RemoteName: "origin"})
+	// CheckIfError(err)
+
+	// fetch (origin by default)
+	r.Fetch(&git.FetchOptions{RemoteName: "origin/master"})
+
+	// Print the latest commit that was just pulled
+	ref, err := r.Head()
+	CheckIfError(err)
+
+	// git reset --hard origin/master
+	w.Reset(&git.ResetOptions{Mode: git.HardReset, Commit: ref.Hash()})
+	err = w.Pull(&git.PullOptions{RemoteName: "origin"})
+	CheckIfError(err)
+
+	commit, err := r.CommitObject(ref.Hash())
+	CheckIfError(err)
+
+	fmt.Println(commit)
 }
