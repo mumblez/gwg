8004771a9146eb4ca030ad3db06f734bf271f2b2
Yusuf Tran <yusuf.tran@karhoo.com>
2018-01-27 18:17:29 +0000

    working poc

--- main.go
+++ main.go
@@ -6,10 +6,16 @@ import (
 
 	"gopkg.in/src-d/go-git.v4"
 	. "gopkg.in/src-d/go-git.v4/_examples"
+	"gopkg.in/src-d/go-git.v4/plumbing"
+	// "gopkg.in/src-d/go-git.v4/plumbing/storer"
 )
 
 // Pull changes from a remote repository
 func main() {
+	// TODO
+	// - break out into function
+	// pass in path (repo dir), ssh priv key location
+	// - create ssh auth connection
 	CheckArgs("<path>")
 	path := os.Args[1]
 
@@ -21,25 +27,43 @@ func main() {
 	w, err := r.Worktree()
 	CheckIfError(err)
 
-	// Pull the latest changes from the origin remote and merge into the current branch
-	// Info("git pull origin")
-	// err = w.Pull(&git.PullOptions{RemoteName: "origin"})
-	// CheckIfError(err)
+	// Fetch latest commits
+	fmt.Println("Fetching...")
+	err = r.Fetch(&git.FetchOptions{RemoteName: "origin"})
+	if err == git.NoErrAlreadyUpToDate {
+		fmt.Println("No new commits")
+		os.Exit(0)
+	}
 
-	// fetch (origin by default)
-	r.Fetch(&git.FetchOptions{RemoteName: "origin/master"})
+	// Get local and remote refs to compare hashes before we proceed
+	remoteRef, err := r.Reference(plumbing.ReferenceName("refs/remotes/origin/master"), true)
+	//remoteRef, err := r.Reference(plumbing.ReferenceName("refs/remotes/origin/HEAD"), true)
+	CheckIfError(err)
+	localRef, err := r.Reference(plumbing.ReferenceName("HEAD"), true)
+	CheckIfError(err)
+	// Exit if there's no need to update
+	if remoteRef.Hash() == localRef.Hash() {
+		fmt.Println("Already up to date")
+		os.Exit(0)
+	}
 
-	// Print the latest commit that was just pulled
-	ref, err := r.Head()
+	// git reset --hard [origin/master|hash]
+	fmt.Println("Resetting work tree....")
+	err = w.Reset(&git.ResetOptions{Mode: git.HardReset, Commit: remoteRef.Hash()})
 	CheckIfError(err)
 
-	// git reset --hard origin/master
-	w.Reset(&git.ResetOptions{Mode: git.HardReset, Commit: ref.Hash()})
-	err = w.Pull(&git.PullOptions{RemoteName: "origin"})
+	// confirm changes
+	fmt.Println("Confirming changes....")
+	headRef, _ := r.Reference(plumbing.ReferenceName("HEAD"), true)
 	CheckIfError(err)
+	if headRef.Hash() == remoteRef.Hash() {
+		fmt.Println("Success")
+	}
 
-	commit, err := r.CommitObject(ref.Hash())
+	// print latest commit
+	commit, err := r.CommitObject(headRef.Hash())
 	CheckIfError(err)
-
 	fmt.Println(commit)
+	fmt.Println(headRef.Hash())
+
 }
