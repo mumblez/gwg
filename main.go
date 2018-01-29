5ba382059ddcfd409e40cd36c85fdf7826ceb0f9
Yusuf Tran <yusuf.tran@karhoo.com>
2018-01-29 13:56:26 +0000

    fix branch validation

--- main.go
+++ main.go
@@ -254,7 +254,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
 	switch e := event.(type) {
 	case *github.PushEvent:
-		if C.Repos[idx].URL == *e.Repo.SSHURL && C.Repos[idx].Branch == strings.TrimPrefix(*.Ref, "/refs/heads/") {
+		if C.Repos[idx].URL == *e.Repo.SSHURL && C.Repos[idx].Branch == strings.TrimPrefix(*e.Ref, "/refs/heads/") {
 			if _, err := os.Stat(C.Repos[idx].Directory); err != nil {
 				go C.Repos[idx].clone()
 			} else {
